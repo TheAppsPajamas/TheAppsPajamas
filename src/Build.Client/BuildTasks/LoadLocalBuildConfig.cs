9d5929dd08146188bba8c27b2362bfe053801b43
Dean Marcussen <dean@theappspajamas.com>
2017-12-17 18:39:46 +0000

    added tasks, son reference, load remote is beginning to come together

---
+++ src/Build.Client/BuildTasks/LoadLocalBuildConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+
+namespace Build.Client.BuildTasks
+{
+    public class LoadLocalBuildConfig : Task
+    {
+        public LoadLocalBuildConfig()
+        {
+        }
+
+        //this should load the local config if it exists
+        //or apply a false if it doesn't exist
+        //which should trigger LoadRemoteBuildConfig
+        public override bool Execute()
+        {
+            return true;
+        }
+    }
+}

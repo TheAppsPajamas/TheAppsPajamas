a2aea2e2351ecf21e201250522cfebb953716ef9
Dean Marcussen <dean@theappspajamas.com>
2017-12-17 09:39:20 +0000

    copied over files from test droid task project I was running

---
+++ src/Build.Client/BuildTasks/GetDroidAppIcons.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Build.Client.BuildTasks
+{
+    public class GetDroidAppIcons : Task
+    {
+        public GetDroidAppIcons()
+        {
+        }
+
+        [Output]
+        public ITaskItem[] OutputFiles { get; set; }
+
+
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Get Droid App Icons started");
+
+            var itemMetadata = new Dictionary<string, string>();
+
+            itemMetadata.Add("LogicalName", "logical-name.png");
+            OutputFiles = new ITaskItem[] {
+                new TaskItem("Resources\\mipmap-hdpi\\Icon.png"
+                             , new Dictionary<string, string>{ {"LogicalName", "mipmap-hdpi\\Icon.png"}}),
+                new TaskItem("..\\mipmap-xxhdpi\\Icon.png"
+                             , new Dictionary<string, string>{ {"LogicalName", "mipmap-xxhdpi\\Icon.png"}})
+                };
+
+            return true;
+        }
+    }
+
+
+}

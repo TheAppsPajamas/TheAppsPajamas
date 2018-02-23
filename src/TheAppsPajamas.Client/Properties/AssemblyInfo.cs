756f593da230e0ee3c49d48164ff62765c248f7e
Dean Marcussen <dean@theappspajamas.com>
2018-02-23 08:43:21 +0000

    and that complete it - rebuilding correctly again now

--- src/TheAppsPajamas.Client/Properties/AssemblyInfo.cs
+++ src/TheAppsPajamas.Client/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Build.Client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TheAppsPajamas.Client")]
+[assembly: AssemblyDescription("TheAppsPajamas Build Client Tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("TheAppsPajamas")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("(c) Dean Marcussen")]
+[assembly: AssemblyCopyright("(c 2018) TheAppsPajamas")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
